431dc7d2aa396fc32eaef389b8fe6889a6099b0e
alenovanwiradhika <ale@bdn.id>
2024-07-16 06:35:45 +0700

    update

--- lib/helper/background_task.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_background_service/flutter_background_service.dart';
-
-void backgroundTask() {
-  FlutterBackgroundService().invoke("Start time",
-    {"current_time": DateTime.now().toIso8601String()},
-  );
-}
